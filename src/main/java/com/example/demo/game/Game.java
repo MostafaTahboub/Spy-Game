57ce8539365539dbad84fe3b71769a6dafa425ce
Mohammad Taradeh <98043145+Mohammad-taradeh@users.noreply.github.com>
2024-09-08 14:18:28 +0300

    create the Game entity

---
+++ src/main/java/com/example/demo/game/Game.java
@@ -0,0 +1,42 @@
+package com.example.demo.game;
+
+import com.example.demo.guess.Guess;
+import com.example.demo.utilities.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.envers.AuditTable;
+import org.hibernate.envers.Audited;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+@SuperBuilder
+@Table(name = "guesses")
+@AllArgsConstructor
+@NoArgsConstructor
+@Audited
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Game extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column
+    private String password;
+
+    @Column
+    private LocalDateTime startsAt;
+
+    @Column
+    private LocalDateTime endsAt;
+
+    @OneToMany
+    private List<Guess> guesses;
+}
