1a1de7f2926310fa35d2df112ffeabd3979e4010
MOSTAFA <201129@ppu.edu.ps>
2024-09-08 13:13:32 +0300

    Create User entity

--- src/main/java/com/example/demo/user/User.java
+++ src/main/java/com/example/demo/user/User.java
@@ -1,7 +1,11 @@
 package com.example.demo.user;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jdk.jfr.Unsigned;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +13,7 @@ import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.envers.AuditTable;
 import org.hibernate.envers.Audited;
+import org.hibernate.validator.constraints.UniqueElements;
 
 @Entity
 @Setter
@@ -21,6 +26,28 @@ import org.hibernate.envers.Audited;
 @AuditTable(value="AUD_User")
 
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
 
+    @NotBlank
+    @Column
+    @UniqueElements
+    private String name;
+
+    @Column
+    @NotBlank
+    @Email(regexp = "[A-Za-z0-9_.-]+@gmail\\.com$", message = "Invalid E-mail format")
+    private String email;
+
+    @Column
+    @NotBlank
+    @Pattern(regexp = "^(?=.[a-z])(?=.[A-Z])(?=.[0-9])(?=.[!@#$%^&])[a-zA-Z0-9!@#$%^&]{8}$")
+    private String password;
+
+    // many to many with Games Entity
+//    @ManyToMany
+
+    // many to many with Guess Entity
 
 }
