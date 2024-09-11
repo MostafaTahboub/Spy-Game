74969303cb8eed409e6f147583e12fa5113e7b8a
MOSTAFA <201129@ppu.edu.ps>
2024-09-11 19:10:44 +0300

    Create user Request

---
+++ src/main/java/com/example/demo/user/UserRequest.java
@@ -0,0 +1,32 @@
+package com.example.demo.user;
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRequest {
+
+    @NotBlank(message = "Name cannot be blank")
+    private String name;
+
+    @Email(regexp = "[A-Za-z0-9_.-]+@gmail\\.com$", message = "Invalid E-mail format")
+    private String email;
+
+    @NotBlank(message = "Password must not be blank")
+    @Size(min = 8, message = "Password length must be at least 8")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$",
+            message = "Password must contain uppercase, lowercase, number and special character")
+    private String password;
+
+//    @FutureOrPresent(message = "Date cannot be in the past")
+//    private LocalDate createdAt;
+    
+
+}
