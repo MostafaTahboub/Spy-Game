33c5c89c9ffe39653b712426c6a78a101e6578d3
Mohammad Taradeh <98043145+Mohammad-taradeh@users.noreply.github.com>
2024-09-16 22:25:33 +0300

    configure the openai api but needs some fixes

---
+++ src/main/java/com/example/demo/chatgpt/ChatRequest.java
@@ -0,0 +1,27 @@
+package com.example.demo.chatgpt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatRequest {
+    private String model;
+    private List<Message> messages;
+    private int n;
+    private double temperature;
+
+    public ChatRequest(String model, String prompt) {
+        this.model = model;
+        this.n = 10;
+        this.messages = new ArrayList<>();
+        this.messages.add(new Message("user", prompt));
+    }
+}
